fc1e7fa95835af423bc9f43a4c337661e27d7a08
Luke Snavely <snavetrain@gmail.com>
2018-03-26 15:27:05 -0500

    fibonacci added

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 tasks = [
     {
-        'id': md5,
+        'id': 'md5',
         'title': "md5",
         'description': 'hash algorithm', 
         'done': True
     },
     {
-        'id': factorial,
-        'title': ,
-        'description': , 
+        'id': 'factorial',
+        'title': 'gfgf',
+        'description': '', 
         'done': False
     },
     {
-        'id': fibonacci,
-        'title': ,
-        'description': , 
+        'id': 'fibonacci',
+        'title':'' ,
+        'description': '', 
+        'done': True
+    },
+    {
+        'id': 'is-prime',
+        'title':'' ,
+        'description':'' , 
         'done': False
     },
     {
-        'id': is-prime,
-        'title': ,
-        'description': , 
-        'done': False
-    },
-    {
-        'id': slack-alert,
-        'title': ,
-        'description': , 
+        'id': 'slack-alert',
+        'title':'' ,
+        'description':'' , 
         'done': False
     }
 ]
@@ -39,7 +39,16 @@ tasks = [
 def get_tasks():
     return jsonify({'tasks': tasks})
 
-@app.route
+@app.route('/fibonacci/<int:x>', methods=['GET'])
+def getFibonacci(x):
+    x = int(x)
+    if x == 0: 
+        return 0
+    elif x == 1: 
+        return 1
+    else: 
+        return getFibonacci(x-1) + getFibonacci(x-2)
+
 
 
 
