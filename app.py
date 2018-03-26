863915efb590620f1bc8f08429a6d89da3ceae31
Luke Snavely <snavetrain@gmail.com>
2018-03-26 14:41:14 -0500

    added tasks

--- app.py
+++ app.py
@@ -2,12 +2,42 @@ from flask import Flask
 
 app = Flask(__name__)
 
+tasks = [
+    {
+        'id': md5,
+        'title': "md5",
+        'description': 'hash algorithm', 
+        'done': False
+    },
+    {
+        'id': factorial,
+        'title': ,
+        'description': , 
+        'done': False
+    },
+    {
+        'id': fibonacci,
+        'title': ,
+        'description': , 
+        'done': False
+    },
+    {
+        'id': is-prime,
+        'title': ,
+        'description': , 
+        'done': False
+    },
+    {
+        'id': slack-alert,
+        'title': ,
+        'description': , 
+        'done': False
+    }
+]
 
-
-
-
-
-
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 
 
